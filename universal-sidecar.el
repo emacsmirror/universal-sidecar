d794418d86a078b59cdf2b21d1d34feafd5eca21
Samuel W. Flint <swflint@flintfam.org>
2023-05-11 10:22:21 -0500

    Add initial universal-sidecar.el file

---
+++ universal-sidecar.el
@@ -0,0 +1,37 @@
+;;; universal-sidecar.el --- A universal sidecar buffer for Emacs -*- lexical-binding: t -*-
+
+;; Copyright (C) 2023 Samuel W. Flint <me@samuelwflint.com>
+
+;; Author: Samuel W. Flint <me@samuelwflint.com>
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;; URL: https://git.sr.ht/~swflint/emacs-universal-sidecar
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "26.1") (magit-section "3.0.0"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+
+(provide 'universal-sidecar)
+
+;;; universal-sidecar.el ends here
