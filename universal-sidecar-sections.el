b09be761ce9d8c218cb37d3e16b38cfd79149d91
Samuel W. Flint <swflint@flintfam.org>
2023-06-25 20:47:25 -0500

    Fix package-requires issue

--- universal-sidecar-sections.el
+++ universal-sidecar-sections.el
@@ -6,7 +6,6 @@
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;; URL: https://git.sr.ht/~swflint/emacs-universal-sidecar
 ;; Version: 0.0.1
-;; Package-Requires: ((emacs "26.1") (universal-sidecar "1.0.0"))
 
 ;; This file is NOT part of GNU Emacs.
 
