9998eea0cff6216acfc354a99431c4506dec1839
Samuel W. Flint <swflint@flintfam.org>
2024-02-03 10:18:29 -0600

    Switch org-cite-sidecar to using universal-sidecar-citeproc

--- org-cite-sidecar.el
+++ org-cite-sidecar.el
@@ -6,9 +6,8 @@
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;; Homepage: https://git.sr.ht/~swflint/emacs-universal-sidecar
 ;; Keywords: bib, org
-;; Version: 0.5.0
-;; Package-Requires: ((emacs "28.1") (citeproc "0.9.4") (universal-sidecar "1.5.0"))
-;; TODO Switch to universal-sidecar-citeproc
+;; Version: 1.0.0
+;; Package-Requires: ((emacs "28.1") (citeproc "0.9.4") (universal-sidecar "1.5.0") (universal-sidecar-citeproc "1.0.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -30,87 +29,54 @@
 ;; and can be shown quite flexibly.  A minimum configuration is as
 ;; follows:
 ;;
-;; (setq org-cite-sidecar-locales "~/.emacs.d/csl-data/locales/"
+;; (setq universal-sidecar-citeproc-locales "~/.emacs.d/csl-data/locales/"
 ;;       ;; set to your directories for locale and style data
-;;       org-cite-sidecar-styles "~/.emacs.d/csl-data/styles/")
+;;       universal-sidecar-citeproc-styles "~/.emacs.d/csl-data/styles/")
 ;; (add-to-list 'universal-sidecar-sections 'org-cite-sidecar)
 ;;
-;; It is important to set the `org-cite-sidecar-locales' and
-;; `org-cite-sidecar-styles' variables to the directories where you
-;; have cloned the CSL locale and style data repositories (see
-;; docstrings for links).
+;; It is important to set the `universal-sidecar-citeproc-locales' and
+;; `universal-sidecar-citeproc-styles' variables to the directories
+;; where you have cloned the CSL locale and style data repositories
+;; (see docstrings for links).
 ;;
 ;; Additionally, there are two arguments to the section which are not
 ;; exposed as customization variables:
 ;;
 ;; - `:style' allows you to select a prefered CSL style within
-;;   `org-cite-sidecar-styles'.  Default is `ieee.csl'.
+;;   `universal-sidecar-citeproc-styles'.  Default is
+;;   `universal-sidecar-citeproc-default-style'.
 ;; - `:header' allows you to change the header of the section from the
 ;;   default "References".
 
 
 ;;; Code:
 
+(require 'citeproc)
 (require 'universal-sidecar)
+(require 'universal-sidecar-citeproc)
 (require 'org)
 (require 'org-element)
 (require 'oc)
 (require 'ol)
-(require 'citeproc)
-
-
-;;; Customization
-
-(defgroup org-cite-sidecar '()
-  "Show org citations in sidecar."
-  :group 'org-cite
-  :group 'universal-sidecar
-  :prefix "org-cite-sidecar-"
-  :link '(url-link :tag "Sourcehut" "https://git.sr.ht/~swflint/emacs-universal-sidecar")
-  :link '(emacs-library-link :tag "Library Source" "org-cite-sidecar.el"))
-
-(defcustom org-cite-sidecar-locales "~/citeproc/locales/"
-  "Path to the directory containing CSL locales data.
-
-Citeproc locales data can be found at
-https://github.com/citation-style-language/locales."
-  :link '(url-link :tag "CSL Locales Repo" "https://github.com/citation-style-language/locales")
-  :type 'directory
-  :group 'org-cite-sidecar)
-
-(defcustom org-cite-sidecar-styles "~/citeproc/styles/"
-  "Path to the directory containing CSL style files.
-
-Citeproc locales data can be found at
-https://github.com/citation-style-language/styles."
-  :link '(url-link :tag "CSL Locales Repo" "https://github.com/citation-style-language/styles")
-  :type 'directory
-  :group 'org-cite-sidecar)
 
 
 ;;; Define the sidecar
 
-(universal-sidecar-define-section org-cite-sidecar ((style "ieee.csl")
+(universal-sidecar-define-section org-cite-sidecar (style
                                                     (header "References"))
                                   (:major-modes (org-mode))
   "Show current buffer citations in Sidecar.
 
-Select reference style by naming a CSL file in STYLE (must be
-found in `org-cite-sidecar-styles'), and the section title using
-HEADER.
-
-Note: It is necessary to also customize the location of locales
-data, `org-cite-sidecar-locales'."
+Select reference style by naming a CSL file in STYLE to override
+`universal-sidecar-citeproc-default-style' (which see), and
+section title using HEADER."
   (when-let* ((data-sources (mapcar #'expand-file-name (org-cite-list-bibliography-files)))
               (references (org-element-map (with-current-buffer buffer
                                              (org-element-parse-buffer))
                               'citation-reference
                             (lambda (citation)
                               (org-element-property :key citation))))
-              (item-getter (citeproc-hash-itemgetter-from-any data-sources))
-              (locale-getter (citeproc-locale-getter-from-dir org-cite-sidecar-locales))
-              (processor (citeproc-create (file-name-concat org-cite-sidecar-styles style)
-                                          item-getter locale-getter)))
+              (processor (universal-sidecar-citeproc-get-processor data-sources :style style)))
     (citeproc-add-uncited references processor)
     (with-current-buffer sidecar
       (universal-sidecar-insert-section org-cite-sidecar header
